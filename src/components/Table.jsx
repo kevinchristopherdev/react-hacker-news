5bbcc2119fc7c7f631e7fdf206e94b2e022ce5cc
GoldenstarUpwork <juvencocastro@gmail.com>
2018-08-10 15:20:53 -0600

    refactored components to be functional rather than class components

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './Button';
 
 const isSearched = (searchTerm) => item =>
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-          {list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID} className={"list-item"}>
-              <h2>
-                  <a href={item.url}>{item.title}</a>
-              </h2>
-              <h3>{item.author}</h3>
-              <h4>Comments:{item.num_comments}</h4>
-              <h4>Popularity Points:{item.points}</h4>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                  Dismiss
-              </Button>
-            </div>
-          )}
-      </div>
-    )
-  }
-}
+const Table = ({ list, pattern, onDismiss }) =>
+    <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID} className={"list-item"}>
+            <h2>
+                <a href={item.url}>{item.title}</a>
+            </h2>
+            <h3>{item.author}</h3>
+            <h4>Comments:{item.num_comments}</h4>
+            <h4>Popularity Points:{item.points}</h4>
+            <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+            </Button>
+          </div>
+        )}
+    </div>
 
 export default Table;
