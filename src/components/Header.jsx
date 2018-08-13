0edb1758a04f4da1b1948529a4bc1152014dda90
Weston Gibler <i-weston.gibler@fourwindsinteractive.com>
2018-08-13 14:02:33 -0600

    initialized a local cache for seach results

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Header = () => 
-    <div className="title-header">
-      <h1>
-        Who KnewZ?
-      </h1>
-    </div>
+  <div className="title-header">
+    <h1>
+      Hacker Knewz
+    </h1>
+  </div>
 
 export default Header;
